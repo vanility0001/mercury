8effc235802474c872f7dd0f803cd8b84db15729
vanility0001 <69370170+vanility0001@users.noreply.github.com>
2021-08-16 15:47:28 +0100

    Delete database directory

--- database/models.go
+++
@@ -1,9 +0,0 @@
-package database
-
-type User struct {
-	Name     string `json:"name"`
-	Password string `json:"password"`
-	Country  string `json:"country"`
-	UserID   int    `json:"userid"`
-	Address  string `json:"address"`
-}
